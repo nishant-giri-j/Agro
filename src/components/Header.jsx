f51f94ebb05fa5d4671a2c2fa6c08c40faf96263
nishant giri <giri.nishant2005@gmail.com>
2026-06-02 12:46:51 +0530

    fix: resolve case sensitivity error for agro.png import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import Logo from "../assets/Agro.png";
+import Logo from "../assets/agro.png";
 
 const MotionLink = motion(Link);
 
